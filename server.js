0dadb4e619572e1a264f3349ddc687487a8f9da1
Roanan Platt <roanan.platt@gmail.com>
2022-07-13 18:45:50 -0600

    server

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const express = require("express");
-//const logger = require("morgan");
 const mongoose = require("mongoose");
-//const compression = require("compression");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const {
-//   Note
-// } = require('./models');
-
-// app.use(logger("dev"));
-
-// app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -26,7 +17,6 @@ useFindAndModify: false,
 mongoose.set('useCreateIndex', true);
 mongoose.set('debug', true);
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.post('/api/submit', ({
@@ -36,6 +26,7 @@ app.post('/api/submit', ({
     .then(dbNote => {
       res.json(dbNote);
     })
+
     .catch(err => {
       res.json(err);
     });
@@ -46,6 +37,7 @@ app.get('/api/all', (req, res) => {
     .then(dbNote => {
       res.json(dbNote);
     })
+    
     .catch(err => {
       res.json(err);
     });
