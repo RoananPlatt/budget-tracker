d243b80bc0d74703be732dba82c86036788dd33d
Roanan Platt <roanan.platt@gmail.com>
2022-07-13 18:27:38 -0600

    transaction name value date

--- models/transaction.js
+++ models/transaction.js
@@ -5,15 +5,18 @@ const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema(
   {
+
     name: {
       type: String,
       trim: true,
       required: "Enter a name for transaction"
     },
+
     value: {
       type: Number,
-      required: "Enter an amount"
+      required: "Enter an amount for transaction"
     },
+    
     date: {
       type: Date,
       default: Date.now
